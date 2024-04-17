3642078a8c8ec1f6a31c591eb724928c7a27fe6a
MUSKAN <muskanprajapati0609@gmail.com>
2024-04-17 11:06:06 +0530

    assignment1

--- assignment1/Q4.py
+++ assignment1/Q4.py
@@ -4,9 +4,12 @@ num1 = int(input("Enter the number 1: "))
 num2 = int(input("Enter the number 2: "))
 num3 = int(input("Enter the number 3: "))
 
-if (num1 > num2) | (num1 > num3):
-    print(f"num1 is greater")
-elif num2>num3:
-    print(f"num2 is greater")
-else :
-    print(f"num3 is greater")
+def max(n1,n2,n3):    
+    if (n1 > n2) | (n1 > n3):
+        print(f"num1 is greater")
+    elif n2>n3:
+        print(f"num2 is greater")
+    else :
+        print(f"num3 is greater")
+
+max(num1,num2,num3)
